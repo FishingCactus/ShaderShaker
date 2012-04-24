d373eed2057364ce96767d587960bea3146f905f
julien.hamaide <julien.hamaide@fishingcactus.com>
2012-04-24 17:06:29 +0200

    Support for swizzle

--- src/language/types.lua
+++ src/language/types.lua
@@ -1,19 +1,46 @@
 Language = Language or {}
 
+Language.IsValidSwizzle = function( swizzle, type )
+	local position_swizzle = "xyzw"
+	local color_swizzle = "rgba"
+	local parameter_count = tonumber( string.sub( string.reverse( type ), 1, 1 ) );
+	
+	parameter_count = parameter_count or 1;
+	
+	return
+		( string.match( swizzle, "[" ..string.sub( position_swizzle, 1, parameter_count ) .. "]*" ) == swizzle )
+		or ( string.match( swizzle, "[" .. string.sub( color_swizzle, 1, parameter_count ) .. "]*" )  == swizzle )
+end
+
 Language.VectorMetatable = {
 	__add = function( a, b ) 
 		assert( a.type == b.type );
  		local result = { type = a.type, node="add", arguments={a,b} }
- 		setmetatable( result, getmetatable( a ) )
+ 		setmetatable( result, Language.VectorMetatable )
  		return result
 	end,
 	
 	__mul = function( a, b ) 
 		assert( type(a) == "number" or type(b) == "number" or  a.type == b.type );
  		local result = { type = ( type(a) == "number" and b.type ) or a.type, node="mul", arguments={a,b} }
- 		setmetatable( result, getmetatable( ( type(a) == "number" and b ) or a ) )
+ 		setmetatable( result, Language.VectorMetatable )
  		return result
-	end	
+	end,
+	
+	__newindex = function( table, key, value )
+		error( "No support for swizzled assignment yet", 2 )
+	end,
+	
+	__index = function( vector, key )
+	
+		if not Language.IsValidSwizzle( key, vector.type ) then
+			error( "Invalid swizzle", 2 );
+		end
+		
+		local result = { type = "float"..tonumber( string.len(key) ), node="swizzle", arguments={ vector, key } }
+		setmetatable( result, Language.VectorMetatable )
+		return result
+	end
 	
 }
 
