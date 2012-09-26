bf826c02bf310c87b5fc6ed5e86d60449e3caa20
julien.hamaide <julien.hamaide@fishingcactus.com>
2012-09-26 12:45:58 +0200

    Some formatting

--- src/generator/hlsl_generator.lua
+++ src/generator/hlsl_generator.lua
@@ -1,3 +1,4 @@
+local i
 HLSLGenerator = {
 
     ["ProcessAst"] = function( ast )
@@ -7,7 +8,7 @@ HLSLGenerator = {
                 error( "No printer for ast node '" .. value.name .. "' in HLSL printer", 1 )
             end
 
-            
+            i = 0
             ShaderPrint( HLSLGenerator[ "process_" .. value.name ]( value ) )
             
         end
@@ -17,7 +18,7 @@ HLSLGenerator = {
     ["ProcessNode"] = function( node )
     
         if HLSLGenerator[ "process_" .. node.name ] == nil then
-                error( "No printer for ast node '" .. node.name .. "' in HLSL printer", 1 )
+            error( "No printer for ast node '" .. node.name .. "' in HLSL printer", 1 )
         end
 
         return HLSLGenerator[ "process_" .. node.name ]( node );
@@ -29,8 +30,12 @@ HLSLGenerator = {
     
         local output
         local function_body_index
+        local prefix = string.rep( [[    ]], i )
+        local previous_i = i
         
-        output = function_node[ 1 ][ 1 ] .. " " .. function_node[ 2 ][ 1 ] .. "(\n"
+        i = i + 1
+        
+        output = prefix .. function_node[ 1 ][ 1 ] .. " " .. function_node[ 2 ][ 1 ] .. "(\n"
         
         if function_node[ 3 ].name == "argument_list" then
             function_body_index = 4
@@ -39,46 +44,54 @@ HLSLGenerator = {
             fuction_body_index = 3
         end
         
-        output = output .. ")\n{\n"
+        output = output .. '\n' .. prefix .. ')\n' .. prefix .. '{\n'
         
         for _, statement in ipairs( function_node[ function_body_index ] ) do
         
-            output = output .. HLSLGenerator.ProcessNode( statement ) .. "\n"
+            output = output .. HLSLGenerator.ProcessNode( statement ) .. '\n'
         end
+        
+        i = previous_i
 
-        return output .. "}\n"
+        return output .. prefix .. '}\n'
         
     end,
     
     ["process_argument_list"] = function( argument_list )
         local output
         local result = {}
+        local prefix = string.rep( [[    ]], i )
         
         for _,argument in ipairs( argument_list ) do
             result[ _ ] = HLSLGenerator.process_argument( argument )
         end
         
-        return table.concat( result, ',\n' );
+        return prefix .. table.concat( result, ',\n' .. prefix );
     end,
     
     ["process_argument"] = function( argument )
-    
+
         return argument[ 1 ][ 1 ] .. ' ' .. argument[ 2 ][ 1 ]
     end,
     
     [ "process_variable_declaration" ] = function( node )
-            
-        local output = node[1][1] .. '\n'
-        
+
+        local prefix = string.rep( [[    ]], i )
+        local output = prefix .. node[1][1] .. '\n'
         local index = 2
+        local previous_i = i
+        
+        i = i +1
         
         while node[index] ~= nil do
         
+            local prefix = string.rep( [[    ]], i )
+            
             if index ~= 2 then
                 output = output .. ',\n'
             end
             
-            output = output .. node[ index ][ 1 ]
+            output = output .. prefix .. node[ index ][ 1 ]
             
             if node[ index ][2] ~= nil then
                 output = output .. '=' .. HLSLGenerator.ProcessNode( node[ index ][ 2 ] )
@@ -86,14 +99,18 @@ HLSLGenerator = {
             
             index = index + 1
         end
+        
+        i = previous_i
+        
         return output .. ';'
     end,
     
     ["process_return"] = function( node )
+        local prefix = string.rep( [[    ]], i )
         if #node == 0 then
-            return 'return;'
+            return prefix .. 'return;'
         else
-            return 'return ' .. HLSLGenerator.ProcessNode( node[ 1 ] ) .. ';'
+            return prefix .. 'return ' .. HLSLGenerator.ProcessNode( node[ 1 ] ) .. ';'
         end
     end,
     
