3c529e71dd8d303b71ba9ec204ff2c3535db61b9
Michael Delva <michael.delva@gmail.com>
2012-10-12 15:45:02 +0200

    Added f suffix to values used by the saturate to clamp function

--- src/utilities/glsl_helper.lua
+++ src/utilities/glsl_helper.lua
@@ -15,7 +15,7 @@ local
         float4x4 = "mat4",
         tex2D = "texture2D",
         texCUBE = "textureCube",
-        saturate = function( str ) return "clamp( " .. str .. ", 0.0, 1.0)" end
+        saturate = function( str ) return "clamp( " .. str .. ", 0.0f, 1.0f)" end
     }
     
 local
