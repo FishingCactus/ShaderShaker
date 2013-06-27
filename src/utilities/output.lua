b1d1a9694677708f612200d6280680b43873302b
Ramses Ladlani <ramses.ladlani@fishingcactus.com>
2013-06-27 13:55:24 +0200

    use decoda_output() when available

--- src/utilities/output.lua
+++ src/utilities/output.lua
@@ -3,30 +3,34 @@ local stream
 
 function ShaderPrint( ... )
     local argument = {...}
-    
+
     if type( argument[ 1 ] ) == "number" then
         stream:write( string.rep( "\t", argument[1] ) )
         stream:write( argument[ 2 ] )
     else
         stream:write( argument[ 1 ] )
     end
-
 end
 
 function InitializeOutputPrint()
     stream = {
-		text = "",
-		write = function( self, data ) self.text = self.text .. data end,
-		}
+        text = "",
+        write = function( self, data )
+                self.text = self.text .. data
+                if decoda_output then
+                    decoda_output( data )
+                end
+            end,
+        }
 
-	_G.CodeOutput = _G.CodeOutput or {}
-	table.insert( _G.CodeOutput, stream )
+    _G.CodeOutput = _G.CodeOutput or {}
+    table.insert( _G.CodeOutput, stream )
 end
 
 function InitializeOutputFile( file )
 
     stream, error_message = io.open( file, "w" )
-    
+
     if stream == nil then
         error( "Error while opening file : " .. error_message, 2 )
     end
