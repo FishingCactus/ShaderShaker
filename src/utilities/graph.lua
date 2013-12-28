e714c503ae98b2952f780f23d60918b87d24010a
Julien Hamaide <julien.hamaide@fishingcactus.com>
2013-12-28 02:00:23 +0100

    Depth first in graph

--- src/utilities/graph.lua
+++ src/utilities/graph.lua
@@ -49,3 +49,62 @@ function GraphNode:WalkNodes()
         return nil
     end
 end
+
+function GraphNode:WalkNodesDepthFirst()
+
+    local processed_node_table = Set.new{}
+    local node_stack = {}
+    local child_index_stack = {}
+
+    node_stack[ 1 ] = self
+    child_index_stack[ 1 ] = nil
+
+    return function()
+
+        while #node_stack > 0 do
+
+            local current_node = node_stack[ 1 ]
+            local child_index = child_index_stack[ 1 ]
+
+            while 1 do
+
+                local child_node = next( current_node.To, child_index )
+                if child_node == nil then
+
+                    table.remove( node_stack, 1 )
+                    table.remove( child_index_stack, 1 )
+                    return current_node
+                else
+
+                    child_index = child_node
+
+                    if not processed_node_table:contains( child_node )  then
+
+                        processed_node_table:insert( current_node )
+
+                        child_index_stack[ 1 ] = child_index
+
+                        while 1 do
+
+                            local child = next( child_node.To )
+
+                            if child == nil then
+                                return child_node
+                            else
+                                table.insert( node_stack, 1, child_node )
+                                table.insert( child_index_stack, 1, child )
+
+                                child_node = child
+                            end
+
+                        end
+
+                        return child_node
+                    end
+                end
+            end
+        end
+
+        return nil
+    end
+end
