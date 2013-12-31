260dd9cfc8e5a8ed8ef09ac603c9871d1af4927f
Julien Hamaide <julien.hamaide@fishingcactus.com>
2013-12-31 06:17:17 +0100

    test graph iterator

--- spec/graph_spec.lua
+++ spec/graph_spec.lua
@@ -3,9 +3,9 @@ dofile "src/utilities/graph.lua"
 
 describe("GraphNode", function()
 
-  it("detect cycles in the graph only if there is some", function()
-        local a_node, b_node, c_node, d_node, e_node
+    local a_node, b_node, c_node, d_node
 
+    before_each(function()
         a_node = GraphNode.new()
         b_node = GraphNode.new()
         c_node = GraphNode.new()
@@ -14,6 +14,33 @@ describe("GraphNode", function()
         a_node:AddTarget( b_node )
         b_node:AddTarget( c_node )
         c_node:AddTarget( d_node )
+    end)
+
+    it("walks node pre-depth first", function()
+        local expected_walk
+        expected_walk = { a_node, b_node, c_node, d_node }
+
+        local index = 1
+
+        for node in a_node:WalkNodes() do
+            assert.are.equals( expected_walk[ index ], node )
+            index = index + 1
+        end
+    end)
+
+    it("walks node depth first", function()
+        local expected_walk
+        expected_walk = { d_node, c_node, b_node, a_node }
+
+        local index = 1
+
+        for node in a_node:WalkNodesDepthFirst() do
+            assert.are.equals( expected_walk[ index ], node )
+            index = index + 1
+        end
+    end)
+
+    it("detect cycles in the graph only if there is some", function()
 
         assert.are.equals( 0, #a_node:DetectCycles() )
 
